c3a78958defffef9366cdb5dfb9116410c769129
TiagoSilvina <tiagoafsilvina@gmail.com>
2024-11-12 13:08:33 +0000

    lc + wc

--- ex2/ex2.sh
+++ ex2/ex2.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-echo "Primeiras 5 linhas do ficheiro"
 
-head -n 5 lorem.txt
+#echo "Primeiras 5 linhas do ficheiro"
+#head -n 5 lorem.txt
+#echo "Linhas com a palavra 'erro'"
+#grep -i 'erro' lorem.txt
 
-echo "Linhas com a palavra 'erro'"
+read -p "Introduza o nome do ficheiro: " ficheiro
 
-grep -i 'erro' lorem.txt
+if [ -f "$ficheiro" ]; then
+    echo "Nº de linhas: $(wc -l < "$ficheiro")"
+    echo "Nº de palavras: $(wc -c < "$ficheiro")"
+    echo "Primeiras 5 linhas do ficheiro:"
+    head -n 5 "$ficheiro"
+    echo "Linhas com a palavra 'erro':"
+    grep -i "erro" "$ficheiro"
+else
+    echo "Ficheiro não encontrado"
+fi
